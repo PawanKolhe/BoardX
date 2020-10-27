0d3f8e26e332722bd5168648f525ba1ddb1ef168
Pawan Kolhe <contact@pawankolhe.com>
2020-10-27 22:48:51 +0530

    update dragging functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 import { boardState } from "./boardState";
-import { createModal, addCardModalHTML, addListModalHTML } from "./modal";
+import { createModal, addListModalHTML } from "./modal";
 import { createList } from "./list";
 import { createCard } from "./card";
+import { Sortable } from "sortablejs";
 
 // Add sample boards
 createList(boardState, 'To Do');
@@ -12,15 +13,26 @@ createList(boardState, 'Done');
 createCard(boardState, 'To Do', 'Sample Task');
 
 // Make boards draggable
-const dragulaLists = dragula([document.querySelector('#listsContainer')], {
-  direction: 'horizontal',
-  moves: (el, source, handle, sibling) => {
-    if(handle.classList.contains('list__drag')) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+// const dragulaLists = dragula([document.querySelector('#listsContainer')], {
+//   direction: 'horizontal',
+//   moves: (el, source, handle, sibling) => {
+//     if(handle.classList.contains('list__drag')) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// });
+const listsContainer = document.querySelector('#listsContainer');
+const draggableLists = new Sortable(listsContainer, {
+  draggable: '.list',
+  handle: '.list__drag',
+  animation: 150,
+  setData: function (dataTransfer, dragEl) {
+    const img = new Image();
+    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+    dataTransfer.setDragImage(img, 0, 0);
+  },
 });
 
 // Make cards draggable
@@ -28,17 +40,12 @@ const dragulaCards = dragula({
   direction: 'vertical',
   slideFactorX: 7,
   slideFactorY: 7,
+  delay: 1000,
+  delayOnTouchOnly: true,
   isContainer: function (el) {
     return el.classList.contains('list__draggable');
   }
 });
-
-// Add Card handler
-// document.querySelector('#addCardButton').addEventListener('click', (e) => {
-//   createModal('Add Card', addCardModalHTML, (task) => {
-//     document.querySelector('#listsContainer').firstChild.querySelector('.list__draggable').appendChild(createCard(task));
-//   });
-// });
 
 // Add Board handler
 document.querySelector('#addListButton').addEventListener('click', (e) => {
